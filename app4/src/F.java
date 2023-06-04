0647f088bd32f0e3a0b6e9f55f7af3ccfcd87787
Peddaraju <peddarajum15@gmail.com>
2023-06-03 23:44:23 -0500

    correction compile time issue

--- app4/src/F.java
+++ app4/src/F.java
@@ -3,7 +3,7 @@ class F
 	public static void main(String[] args) 
 	{
 		String s1 = args[0];
-		int i = s1;
+		int i = Integer.parseInt(s1);
 		double j = Math.sqrt(i);
 		System.out.println("square root of " + i + " is :" + j);
 	}
